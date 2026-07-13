91a589b583a012a12fe4104e22abbc75898a9e39
Alon <alonso.moros@alumnos.upm.es>
2026-07-13 10:02:56 +0200

    Estructura modular para Maven

--- tfg/subscription-service/src/main/java/com/alonsomoros/tfg/SubscriptionServiceApplication.java
+++ tfg/subscription-service/src/main/java/com/alonsomoros/tfg/SubscriptionServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TfgApplication {
+public class SubscriptionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TfgApplication.class, args);
+		SpringApplication.run(SubscriptionServiceApplication.class, args);
 	}
 
 }
