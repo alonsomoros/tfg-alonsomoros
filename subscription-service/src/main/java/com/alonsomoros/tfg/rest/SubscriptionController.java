e210a80443d80e4b3fd3d62b0ba121bf3a3fe021
Alon <alonso.moros@alumnos.upm.es>
2026-07-17 20:51:57 +0200

    Refactor

--- subscription-service/src/main/java/com/alonsomoros/tfg/rest/SubscriptionController.java
+++ subscription-service/src/main/java/com/alonsomoros/tfg/rest/SubscriptionController.java
@@ -3,7 +3,7 @@ package com.alonsomoros.tfg.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alonsomoros.dtos.SubscriptionRequestDto;
+import com.alonsomoros.dto.SubscriptionRequestDto;
 
 import lombok.extern.slf4j.Slf4j;
 
