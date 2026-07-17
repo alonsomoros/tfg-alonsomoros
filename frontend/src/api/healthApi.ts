6c484c1ce5090db9d0f4dc7f3b098ad5003170ef
Alon <alonso.moros@alumnos.upm.es>
2026-07-17 02:01:16 +0200

    Añadidos Frontend Logs y dependencia LSF4J para Backend Logs

--- frontend/src/api/healthApi.ts
+++ frontend/src/api/healthApi.ts
@@ -4,6 +4,7 @@ export const checkSubscriptionServiceHealth = async () => {
     try {
         // GET a http://localhost:8080/subscriptions/health
         const response = await httpClient.get('/subscriptions/health');
+        console.log('Subscription Service Health Check Response:', response.data);
         return response.data;
     } catch (error) {
         throw new Error('El backend de Subscription Service está caído');
@@ -14,6 +15,7 @@ export const checkRecurringEngineHealth = async () => {
     try {
         // GET a http://localhost:8081/recurring-engine/health
         const response = await httpClient.get('/recurring-engine/health');
+        console.log('Recurring Engine Health Check Response:', response.data);
         return response.data;
     } catch (error) {
         throw new Error('El backend de Recurring Engine está caído');
--- subscription-service/src/main/java/com/alonsomoros/tfg/rest/SubscriptionController.java
+++ subscription-service/src/main/java/com/alonsomoros/tfg/rest/SubscriptionController.java
@@ -2,17 +2,20 @@ package com.alonsomoros.tfg.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
+@Slf4j
 @RequestMapping("/subscriptions")
 @RestController
 public class SubscriptionController {
 
     @GetMapping("/health")
     public String health() {
+        log.info("Health check endpoint called.");
         return "Subscription service is healthy.";
     }
-    
+
 }
